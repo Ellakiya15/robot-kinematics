e011a3572005e2230983e023933f0c1fb9239363
Ellakiya <ellakiyaarun@gmail.com>
2024-08-09 17:07:28 +0530

    revised

--- frwd_ktcs_ex2.py
+++ frwd_ktcs_ex2.py
@@ -6,12 +6,7 @@ import numpy as np
 from numpy import sin,cos,tan
 import math
 
-theta=[]
-for t in range(1,5):
-    t = float(input(f"Enter the theta {t}:"))
-    theta.append(t)
-theta = np.deg2rad(theta)
-theta = np.array(theta)
+
 
 #along x-axis
 dx = []
